4f52cb821cea08fcc5fa953daa034b54ab5dacd3
brookebachman00 <bbachman1994@gmail.com>
2021-01-06 09:23:47 -0700

    cleaned up the imported files

--- src/Client/index.js
+++ src/Client/index.js
@@ -1,20 +1,19 @@
 import {fetchCoordinates} from './js/fetchCoordinates'
-import {postData} from './js/postData.js'
 import {changeDate} from './js/changeDate.js'
 import {changeTime} from './js/changeTime.js'
 import {updateFrontend} from './js/updateFrontend.js'
 import {fetchWeatherDataCurrent} from './js/fetchWeatherDataCurrent.js'
 import {fetchWeatherDataWeek} from './js/fetchWeatherDataWeek.js'
-import {fetchCoordinates} from './js/fetchVacationPhoto.js'
+import {fetchVacationPhoto} from './js/fetchVacationPhoto.js'
+import '../Client/styles/styles.scss'
 
 export {
     fetchCoordinates,
     fetchWeatherDataCurrent,
     fetchWeatherDataWeek,
-    postData,
     changeDate,
     changeTime,
     updateFrontend,
-    fetchVacationPhotos
+    fetchVacationPhoto
 }
 
