93ab544a4e4b64ba3455490fbf2e6790253dc2e2
brookebachman00 <bbachman1994@gmail.com>
2021-01-17 17:45:40 -0700

    working on updatefrontend

--- src/Client/js/updateFrontend.js
+++ src/Client/js/updateFrontend.js
@@ -1,26 +1,28 @@
 const updateFrontend = async (data, lengthTrip) => {
 	console.log("update frontend is running", data)
 	const mainDiv = document.getElementById("main-container")
+	const form = document.getElementById('form-container')
 	const littleDiv = document.createElement('div')
-	const date = document.createElement("p")
 	const location = document.createElement("p")
 	const highTemp = document.createElement("p")
 	const clouds = document.createElement("p")
 	const snow = document.createElement("p")
 	const uv = document.createElement("p")
-	const tripDate = document.createElement('p')
 	mainDiv.appendChild(littleDiv)
-	littleDiv.appendChild(tripDate)
+	mainDiv.removeChild(form)
 	littleDiv.id = "entry-holder"
-	console.log(lengthTrip)
+	littleDiv.appendChild(location)
+	
 	if (lengthTrip <= 1){
-		tripDate.innerHTML = `Your trip is a day trip`
+		location.innerHTML = `Your trip to ${data[0].city_name} is a day trip`
 	} else {
-		tripDate.innerHTML = `Your trip is ${lengthTrip} days long`
+		location.innerHTML = `Your trip to ${data[0].city_name} is ${lengthTrip} days long`
 	}
 	
 	
 }
+
+//function 
 
 
 
