ce2d6a8d0e5a3921c7d69200e15d4fafc33b542a
brookebachman00 <bbachman1994@gmail.com>
2021-01-26 18:28:36 -0700

    combined 2 functions

--- src/Client/js/updateFrontend.js
+++ src/Client/js/updateFrontend.js
@@ -12,9 +12,6 @@ const updateFrontend = async (data, lengthTrip) => {
 	const uv = document.createElement('p');
 	const photo = document.getElementById('picture');
 	mainDiv.appendChild(photo);
-	
-	
-
 	mainDiv.removeChild(form);
 	mainDiv.appendChild(littleDiv);
 	littleDiv.id = 'entry-holder';
@@ -27,13 +24,12 @@ const updateFrontend = async (data, lengthTrip) => {
 	highTemp.id = 'temp';
 	uv.id = 'uv';
 
-  
 	highTemp.innerHTML = `It is ${data[0].temp + 32} degrees today`;
 	uv.innerHTML = `It looks like there are ${data[0].weather.description} today`;
 	if (data[0].snow) {
-		 snow.innerHTML = `There is ${data[0].snow / 0.039} inches per hour falling. Make sure to dress warm! `;
+		snow.innerHTML = `There is ${data[0].snow / 0.039} inches per hour falling. Make sure to dress warm! `;
 	} else {
-		 snow.innerHTML = 'Looks like you are going somewhere warm';
+		snow.innerHTML = 'Looks like you are going somewhere warm';
 	}
 
 	if (lengthTrip <= 1) {
@@ -43,22 +39,21 @@ const updateFrontend = async (data, lengthTrip) => {
 	}
 	photo.innerHTML = `<img src=${searchImages()}>`;
 
-	weatherType(data)
+	weatherType(data);
 };
 
 const weatherType = (data) => {
-	if (data[0].snow > 1){
-		searchImages("snow")
-	} else if (data[0].precip > 3){
-		searchImages("rain")
-	}else if (data[0].clouds > 50){
-		searchImages("clouds")
+	if (data[0].snow > 1) {
+		searchImages('snow');
+	} else if (data[0].precip > 3) {
+		searchImages('rain');
+	} else if (data[0].clouds > 50) {
+		searchImages('clouds');
 	}
 	// } else if (data[0].clouds <= 25){
 	// 	searchImages(small, clouds)
 	// }
-
-}
+};
 
 const searchImages = async (element, second) => {
 	//const element = element;
@@ -67,33 +62,30 @@ const searchImages = async (element, second) => {
 	// } else {
 	// 	element = element;
 	// }
-	
+
 	const apikey = '19614841-f24f6c26e68114f6eb3490d99';
 	const url = `https://pixabay.com/api/?key=${apikey}&q=${element}&image_type=photo`;
 	try {
 		const data = await fetch(url);
 		let response = await data.json();
-		console.log(response)
-		goOverImages(response, element)
+		console.log(response);
+		let newResults = [];
+		let array = response.hits;
+		array.forEach((photoTag) => {
+			let newTags = photoTag.tags.replace(/[^0-9a-zA-Z]/g, '');
+			if (newTags.includes(element)) {
+				newResults.push(photoTag.largeImageURL);
+			}
+		});
+		console.log(newResults[0])
+		return newResults[0];
+
 		//return response.hits[0].largeImageURL;
 	} catch (error) {
 		console.log(error);
 	}
 };
 
-const goOverImages = (data, search) => {
-	console.log(data.hits)
-	let newResults = []
-	let array = data.hits
-	array.forEach(element => {
-		let newElement = element.tags.replace(/[^0-9a-zA-Z]/g, '')
-		if (newElement.includes(search)){
-			newResults.push(element.largeImageURL)
-		}
-	});
-	
-	console.log(newResults[0])
-	return newResults[0]
-}
+
 
 export { updateFrontend };
