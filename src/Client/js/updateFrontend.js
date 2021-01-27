262903074eaebf2bb400c4b0891f6a037746390b
brookebachman00 <bbachman1994@gmail.com>
2021-01-26 18:14:29 -0700

    I am filtering through the images so that they match the terms I am entering"

--- src/Client/js/updateFrontend.js
+++ src/Client/js/updateFrontend.js
@@ -12,10 +12,10 @@ const updateFrontend = async (data, lengthTrip) => {
 	const uv = document.createElement('p');
 	const photo = document.getElementById('picture');
 	mainDiv.appendChild(photo);
-	photo.innerHTML = `<img src=${await searchImages()}>`;
+	
+	
 
 	mainDiv.removeChild(form);
-
 	mainDiv.appendChild(littleDiv);
 	littleDiv.id = 'entry-holder';
 	littleDiv.appendChild(tripLength);
@@ -41,20 +41,59 @@ const updateFrontend = async (data, lengthTrip) => {
 	} else {
 		tripLength.innerHTML = `Your trip to ${data[0].city_name} is ${lengthTrip} days long`;
 	}
+	photo.innerHTML = `<img src=${searchImages()}>`;
+
+	weatherType(data)
 };
 
+const weatherType = (data) => {
+	if (data[0].snow > 1){
+		searchImages("snow")
+	} else if (data[0].precip > 3){
+		searchImages("rain")
+	}else if (data[0].clouds > 50){
+		searchImages("clouds")
+	}
+	// } else if (data[0].clouds <= 25){
+	// 	searchImages(small, clouds)
+	// }
 
-const searchImages = async () => {
-	const element = 'yellow+flower';
+}
+
+const searchImages = async (element, second) => {
+	//const element = element;
+	// if (second){
+	// 	element = element + second
+	// } else {
+	// 	element = element;
+	// }
+	
 	const apikey = '19614841-f24f6c26e68114f6eb3490d99';
 	const url = `https://pixabay.com/api/?key=${apikey}&q=${element}&image_type=photo`;
 	try {
 		const data = await fetch(url);
 		let response = await data.json();
-		return response.hits[0].largeImageURL;
+		console.log(response)
+		goOverImages(response, element)
+		//return response.hits[0].largeImageURL;
 	} catch (error) {
 		console.log(error);
 	}
 };
+
+const goOverImages = (data, search) => {
+	console.log(data.hits)
+	let newResults = []
+	let array = data.hits
+	array.forEach(element => {
+		let newElement = element.tags.replace(/[^0-9a-zA-Z]/g, '')
+		if (newElement.includes(search)){
+			newResults.push(element.largeImageURL)
+		}
+	});
+	
+	console.log(newResults[0])
+	return newResults[0]
+}
 
 export { updateFrontend };
